3b624e911fa863d07175b61cd5c530cf734c854f
Mestery <mestery@pm.me>
2021-03-28 21:18:55 +0200

    mestery d4

---
+++ defi04/javascript/mestery/README.md
@@ -0,0 +1,10 @@
+# Not A Counter
+
+A simple HTTP application built on top of [Not An HTTPServer](https://github.com/mesteery/not-an-httpserver) (simple http/1.0 server)
+
+### Usage
+
+```sh
+$ yarn build
+$ yarn node .
+```
---
+++ defi04/javascript/mestery/src/index.ts
@@ -0,0 +1,147 @@
+import { parse } from 'querystring';
+import { Server, decodeBody } from 'not-an-httpserver';
+
+const counters = new Map<string, number>([['carotte', 10]]);
+// eslint-disable-next-line sonarjs/cognitive-complexity
+const server = new Server((request, response) => {
+	if (request.rawHeaders.get('host') !== 'compteur.notaname.fr:8080') {
+		response.writeHead(400);
+		response.end();
+		return;
+	}
+
+	response.writeHead(200);
+	const body = request.body ? (decodeBody(request.body, 'utf8') as string) : undefined;
+
+	if (request.method === 'GET' && request.url.pathname === '/') {
+		response.headers['Content-Type'] = 'application/json';
+		response.end(
+			JSON.stringify({
+				...Object.fromEntries(counters.entries()),
+				// eslint-disable-next-line unicorn/no-array-reduce
+				etoile: [...counters.values()].reduce((accumulator, current) => accumulator + current),
+			}),
+		);
+	} else if (request.method === 'GET' && request.url.pathname && /^\/[\da-z]+$/.test(request.url.pathname)) {
+		const counterName = request.url.pathname.slice(1);
+		if (counterName === 'etoile') {
+			response.headers['Content-Type'] = 'application/json';
+			response.end(
+				JSON.stringify({
+					name: 'etoile',
+					// eslint-disable-next-line unicorn/no-array-reduce
+					value: [...counters.values()].reduce((accumulator, current) => accumulator + current),
+				}),
+			);
+			return;
+		}
+
+		const counter = counters.get(counterName);
+		if (counter === undefined) {
+			response.writeHead(404);
+			response.end();
+			return;
+		}
+
+		response.headers['Content-Type'] = 'application/json';
+		response.end(JSON.stringify({ name: counterName, value: counter }));
+	} else if (request.method === 'POST' && request.url.pathname === '/') {
+		let names;
+		if (body) {
+			try {
+				names =
+					request.rawHeaders.get('content-type') === 'application/x-www-form-urlencoded'
+						? parse(body).name
+						: JSON.parse(body)?.name;
+				// eslint-disable-next-line no-empty
+			} catch {}
+		}
+
+		if (!names || names.length === 0) {
+			response.writeHead(400);
+			response.end();
+			return;
+		}
+
+		if (typeof names === 'string') {
+			names = [names];
+		}
+
+		const createdCounters: Record<string, number> = {};
+		for (const name of names) {
+			if (!name.trim() || counters.has(name)) {
+				response.writeHead(422, 'Unprocessable Entity');
+				response.end();
+				return;
+			}
+
+			counters.set(name, 0);
+			createdCounters[name] = 0;
+		}
+		response.headers['Content-Type'] = 'application/json';
+		response.end(JSON.stringify(createdCounters));
+	} else if (request.method === 'PUT' && request.url.pathname && /^\/[\da-z]+$/.test(request.url.pathname)) {
+		const counterName = request.url.pathname.slice(1);
+		if (counterName === 'etoile') {
+			response.writeHead(405, 'Not Allowed', { Accept: 'GET' });
+			response.end();
+			return;
+		}
+
+		if (!counters.has(counterName)) {
+			response.writeHead(404);
+			response.end();
+			return;
+		}
+
+		if (!body) {
+			response.writeHead(400);
+			response.end();
+			return;
+		}
+
+		let newValue;
+
+		try {
+			newValue =
+				request.rawHeaders.get('content-type') === 'application/x-www-form-urlencoded'
+					? parse(body).value
+					: JSON.parse(body)?.value;
+
+			newValue = parseInt(Array.isArray(newValue) ? newValue[0] : newValue, 10);
+			// eslint-disable-next-line no-empty
+		} catch {}
+
+		if (!newValue || isNaN(newValue)) {
+			response.writeHead(400);
+			response.end();
+			return;
+		}
+
+		counters.set(counterName, newValue);
+		response.writeHead(204);
+		response.end();
+	} else if (request.method === 'DELETE' && request.url.pathname && /^\/[\da-z]+$/.test(request.url.pathname)) {
+		const counterName = request.url.pathname.slice(1);
+		if (counterName === 'etoile') {
+			response.writeHead(405, 'Not Allowed', { Accept: 'GET' });
+			response.end();
+			return;
+		}
+
+		const deleted = counters.delete(counterName);
+		if (!deleted) {
+			response.writeHead(404);
+			response.end();
+			return;
+		}
+
+		response.writeHead(204);
+		response.end();
+	} else {
+		response.writeHead(404);
+		response.end();
+	}
+});
+
+server.listen(8080, () => console.log('Connected!'));
