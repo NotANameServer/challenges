74341033f8f0c66bd3c74a2477cfc23cbecaf382
Mestery <mestery@pm.me>
2021-07-06 19:54:50 +0200

    Update mestery.mjs

--- defi07/javascript/mestery.mjs
+++ defi07/javascript/mestery.mjs
@@ -1,38 +1,39 @@
+#! Je sais, c'est hideux.
+// Je sais, c'est hideux.
+/* Je sais, c'est hideux. */
+
 import { createInterface } from 'readline';
 
 const GOLDEN_NUMBER = 0.5 + Math.sqrt(5) / 2;
 
 const OPERATORS = {
-  '+': (roperand, loperand) => loperand + roperand,
-  '-': (roperand, loperand) => loperand - roperand,
-  '*': (roperand, loperand) => loperand * roperand,
-  '/': (roperand, loperand) => loperand / roperand,
-  '%': (roperand, loperand) => loperand % roperand,
-  '^': (roperand, loperand) => loperand ** roperand,
-  '!'(operand) {
-    if (!Number.isInteger(operand) || operand < 0) {
+  '+': [(rhs, lhs) => lhs + rhs, 0],
+  '-': [(rhs, lhs) => lhs - rhs, 0],
+  '*': [(rhs, lhs) => lhs * rhs, 1],
+  '/': [(rhs, lhs) => lhs / rhs, 1],
+  '%': [(rhs, lhs) => lhs % rhs, 1],
+  '^': [(rhs, lhs) => lhs ** rhs, 2],
+  '!': [(lhs) => {
+    if (!Number.isInteger(lhs) || lhs < 0) {
       throw new Error('Factorial called on float or negative number.');
     }
     let result = 1;
-    for (let i = 2; i < operand; i++) {
+    for (let i = 2; i < lhs; i++) {
       result *= i;
     }
-    return result * operand;
-  },
-  fibonacci(operand) {
-    if (!Number.isInteger(operand) || operand < 0) {
+    return result * lhs;
+  }],
+  fibonacci: [(n) => {
+    if (!Number.isInteger(n) || n < 0) {
       throw new Error('Fibonacci called on float or negative number.');
     }
-    return Math.trunc((1 / Math.sqrt(5)) * (GOLDEN_NUMBER ** operand - 1 / GOLDEN_NUMBER ** operand));
-  },
-  // aliases
-  '√': Math.sqrt,
-  ln: Math.log,
+    return Math.trunc((1 / Math.sqrt(5)) * (GOLDEN_NUMBER ** n - 1 / GOLDEN_NUMBER ** n));
+  }],
 };
 
 for (const k of Object.getOwnPropertyNames(Math)) {
   if (k !== 'sign' && typeof Math[k] === 'function' && Math[k].length === 1) {
-    OPERATORS[k] = Math[k];
+    OPERATORS[k] = [Math[k]];
   }
 }
 
@@ -50,18 +51,18 @@ function parseNumber(input) {
   return n;
 }
 
-function evalPostfix(postfix) {
+function evalPostfix(tokens) {
   const stack = [];
-  for (const token of postfix) {
+  for (const token of tokens) {
     if (token in OPERATORS) {
       if (stack.length < OPERATORS[token].length) {
         throw new Error('Invalid expression.');
       }
-      stack.push(OPERATORS[token](...Array.from({ length: OPERATORS[token].length }, () => stack.pop())));
+      stack.push(OPERATORS[token][0](...Array.from({ length: OPERATORS[token].length }, () => stack.pop())));
       continue;
     }
 
-    stack.push(NUMBERS[token] ?? parseNumber(token));
+    stack.push(typeof token === 'number' ? token : (NUMBERS[token] ?? parseNumber(token)));
   }
   if (stack.length !== 1) {
     throw new Error('Invalid expression.');
@@ -69,26 +70,13 @@ function evalPostfix(postfix) {
   return stack[0];
 }
 
-function precedence(operator) {
-  if (operator === '^') {
-    return 2;
-  }
-  return operator === '*' || operator === '/';
-}
-
-function* tokenizeInfix(expression) {
+function* shuntingYard(tokens) {
   const opstack = [];
 
-  for (let token of expression.toLowerCase().split(/([\^!*%/()√]|\b\s*[-+])/)) {
-    token = token.trim();
-
-    if (!token) {
-      continue;
-    }
-
+  for (let token of tokens) {
     if (token in OPERATORS) {
       // function
-      if (token.length > 1 || token === '!' || token === '√') {
+      if (token.length > 1 || token === '!') {
         opstack.push(token);
         continue;
       }
@@ -97,8 +85,8 @@ function* tokenizeInfix(expression) {
       while (
         opstack.length &&
         opstack[opstack.length - 1] !== '(' &&
-        (precedence(opstack[opstack.length - 1]) > precedence(token) ||
-          (token !== '^' && precedence(opstack[opstack.length - 1]) === precedence(token)))
+        (OPERATORS[opstack[opstack.length - 1]][1] > OPERATORS[token][1] ||
+          (token !== '^' && OPERATORS[opstack[opstack.length - 1]][1] === OPERATORS[token][1]))
       ) {
         yield opstack.pop();
       }
@@ -129,13 +117,62 @@ function* tokenizeInfix(expression) {
   yield* opstack.reverse();
 }
 
+const OPERATORS_SUBSET = ['+', '-', '*', '/', '^', '%'];
+
+function tokenizeInfix(expression) {
+  const tokens = [];
+
+  for (let i = 0; i < expression.length; i++) {
+    const char = expression[i];
+    if (char === ' ') {
+      continue;
+    }
+
+    if ((char === '+' || char === '-') && (tokens.length === 0 || OPERATORS_SUBSET.concat('(').includes(tokens[tokens.length - 1])) && char[i + 1] !== ' ') {
+      if (char === '-') tokens.push(-1, '*');
+    } else if (char >= '0' && char <= '9') {
+      let number = '';
+      while (expression[i] >= '0' && expression[i] <= '9' || expression[i] === '.') {
+        if (expression[i] === '.' && number.includes('.')) {
+          throw new Error('Invalid expression.');
+        }
+        number += expression[i++];
+      }
+      i--;
+      tokens.push(parseNumber(number));
+    } else if ([...OPERATORS_SUBSET, '!'].includes(char)) {
+      if (char === '!') {
+        if (![')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'e', 'i' /* pi */, 'π'].includes(expression[i - 1])) {
+          throw new Error('Invalid expression.');
+        }
+      }
+      tokens.push(char);
+    } else if (['(', ')', 'e', 'π'].includes(char)) {
+      tokens.push(char);
+    } else if (Object.keys(OPERATORS).some((value) => value.length > 1 && value[0] === char)) {
+      let func = char;
+      while (expression[i + 1].toLowerCase() >= 'a' && expression[i + 1].toLowerCase() <= 'z' || expression[i + 1] >= '0' && expression[i + 1] <= '9') {
+        func += expression[++i];
+      }
+      if (!(func in OPERATORS)) {
+        throw new Error('Invalid expression.');
+      }
+      tokens.push(func);
+    } else {
+      throw new Error('Invalid expression.');
+    }
+  }
+
+  return tokens;
+}
+
 function tokenizePostfix(expression) {
   return expression.toLowerCase().split(/\s+/);
 }
 
 function evalExpression(expression) {
   try {
-    return evalPostfix(tokenizeInfix(expression));
+    return evalPostfix(shuntingYard(tokenizeInfix(expression)));
   } catch (error) {
     try {
       return evalPostfix(tokenizePostfix(expression));
@@ -170,11 +207,11 @@ rl.on('line', (line) => {
       if (line.startsWith('e ')) {
         logResult(evalExpression(line.slice(2)));
       } else if (line.startsWith('ei ')) {
-        logResult(evalPostfix(tokenizeInfix(line.slice(3))));
+        logResult(evalPostfix(shuntingYard(line.slice(3))));
       } else if (line.startsWith('ep ')) {
         logResult(evalPostfix(tokenizePostfix(line.slice(3))));
       } else if (line.startsWith('i2p ')) {
-        logResult([...tokenizeInfix(line.slice(4))].join(' '));
+        logResult([...shuntingYard(line.slice(4))].join(' '));
       } else {
         logResult(evalExpression(line));
       }
